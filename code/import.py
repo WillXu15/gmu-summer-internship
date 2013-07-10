f0a55a9581cabdeb81ef2466bd9c6d4a6f04bdcf
Will Xu <williamxu15@gmail.com>
2013-07-10 09:54:07 -0400

    no longer needed

--- code/import.py
+++ code/import.py
@@ -5,6 +5,4 @@ directory = os.path.dirname(__file__)
 picLoc = os.path.join(directory, "../video-image/1m50s.png")
 
 image = cv2.imread(picLoc)
-cv2.imshow("", image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+print "sending image to houghLines.py"
