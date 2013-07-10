75329b82cd8aee02b78320601f3b92117fff8e1a
Will Xu <williamxu15@gmail.com>
2013-07-10 11:14:15 -0400

    new houghLines working code

--- code/houghLines.py
+++ code/houghLines.py
@@ -1,6 +1,7 @@
 import cv2
 import os
 import math
+import numpy
 
 def main():
 
@@ -9,18 +10,21 @@ def main():
 
 	image = cv2.imread(picLoc)
 	print "sending image to houghLines.py"
-
 	a = hough(image)
 	cv2.imshow("", a)
 	cv2.waitKey(0)
-	cv2.destroyAllChildren()
+	cv2.destroyAllWindows()
 
 def hough(image):
+
 	canny = cv2.Canny(image, 50, 200)
+	color_image = cv2.cvtColor(canny, cv2.COLOR_GRAY2BGR)
 	houghLines = cv2.HoughLinesP(canny, 1, math.pi/180, 50)
-	for x in range(len(houghLines)):
-		pt1 = (houghLines[x][0], houghLines[x][1])
-		pt2 = (houghLines[x][2], houghLines[x][3])
-		cv2.line(image, pt1, pt2, (255,0,0), 3)
-	return image
+	for x in range(len(houghLines[0])):
+		print x
+		pt1 = (houghLines[0][x][0], houghLines[0][x][1])
+		pt2 = (houghLines[0][x][2], houghLines[0][x][3])
+		cv2.line(color_image, pt1, pt2, (0,0,255), 3)
+	return color_image
+
 main()
