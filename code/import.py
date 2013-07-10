d707ee641a76d586935373130ecfe4f8b529a29e
Will Xu <williamxu15@gmail.com>
2013-07-10 08:27:49 -0400

    imported the image

--- code/import.py
+++ code/import.py
@@ -1,1 +1,10 @@
 import cv2
+import os
+
+directory = os.path.dirname(__file__)
+picLoc = os.path.join(directory, "../video-image/1m50s.png")
+
+image = cv2.imread(picLoc)
+cv2.imshow("", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
