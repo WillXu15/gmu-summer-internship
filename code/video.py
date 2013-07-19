bf15e0d4f4d569714213ddcab67207315b5cae23
Will Xu <williamxu15@gmail.com>
2013-07-19 09:47:19 -0400

    video.py creates the write.avi which is a warped version of the original video.

--- code/video.py
+++ code/video.py
@@ -1,18 +1,45 @@
+import cv
 import cv2
-import numpy as np
+import numpy
 import os
 
 def main():
-	
+#============================================Read video in==========================================================
 	directory = os.path.dirname(__file__)
-	vidLoc = os.path.join(directory, "../video-image/video.avi")
+	vidLoc = os.path.join(directory, "../video-image/video.mp4")
 
 	vid = cv2.VideoCapture()
-	vid.release()
-	print vid.open(vidLoc)
+	vid.open(vidLoc)
 
-	if vid.isOpened():
-		print "you're good"
+	vid.set(1,479)
+	
+#=====================================================Corners and Warping==================================================
+	corners = numpy.array([[141,81],[179,663],[1125,527],[1083,86]], numpy.float32)
+	
+	topLength = corners[3][0] - corners[0][0]
+	botLength = corners[2][0] - corners[1][0]
+
+	leftLength = corners[1][1] - corners[0][1]
+	rightLength = corners[2][1] - corners[3][1]
+	
+	avgTop = int((topLength + botLength)/2)
+	avgLeft = int((leftLength + rightLength)/2)
+
+	write = cv2.VideoWriter("write.avi", cv.CV_FOURCC("M", "J", "P", "G"), int(vid.get(5)), (avgTop,avgLeft))
+
+	newcorners = numpy.array([[0,0], [0, avgLeft], [avgTop, avgLeft], [avgTop,0]],numpy.float32)
+
+	dsize = (avgTop, avgLeft)
+#====================================================Warp all frames=====================================================
+	for x in range(2820):
+		debug, im = vid.read()
+
+		transformMatrix = cv2.getPerspectiveTransform(corners, newcorners)
+
+		image = cv2.warpPerspective(im, transformMatrix, dsize)
+		
+		write.write(image)
+	
 	
 main()
 
