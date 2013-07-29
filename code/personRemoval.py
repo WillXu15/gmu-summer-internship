0cc0cc6e7a024b0f0bb0746d8b11a7453a6f8ea6
Will Xu <williamxu15@gmail.com>
2013-07-29 10:52:30 -0400

    personRemoval improved though there are huge memory issues

--- code/personRemoval.py
+++ code/personRemoval.py
@@ -1,72 +1,29 @@
 import cv2
 import numpy as np
+
 def main():
+
 	print "OPENING VIDEO"	
 	vid = cv2.VideoCapture()
 	vid.open("write.avi")
-
-	N = 150
-	imgs = np.asarray([ vid.read()[1] for i in xrange(N) ])
-
+	print "LOADING VIDEO"
+	imgs = np.asarray([ vid.read()[1] for i in range(int(vid.get(7))) ])
+	vid.release()
+	print "CONCATENATING VIDEO"
+	imgs = np.concatenate( ( np.repeat( imgs[:1], 5, axis = 0 ), imgs, np.repeat( imgs[-1:], 5, axis = 0 ) ), axis = 0 )
+	N = 5
+	print "FINDING MEDIAN"
 	## imgs is a num-frames x num-rows x num-cols x num-channels array.
-	median_frame = np.median( imgs, axis=0 )
-	median_frame = median_frame.astype(np.uint8)
-
-	cv2.imshow("", median_frame)
+	median_frame = np.median( imgs[N-5:N+6], axis=0 )
+	frame_num = 0
+	cv2.imshow(str(frame_num), median_frame.astype(np.uint8))
 	key = cv2.waitKey()
 
-"""
 	while key != 113:
-				
-		imgs[:-1] = imgs[1:]
-		imgs[-1] = vid.read()[1]
-		median_frame = np.median( imgs, axis=0 )
-		cv2.imshow(str(frame_num), median_frame.as_opencv_image() )
-	cv2.destroyWindow("")
-"""
+		frame_num += 1
+		N+=1
+		median_frame = np.median( imgs[N-5:N+6], axis=0 )
+		cv2.imshow(str(frame_num), median_frame.astype(np.uint8) )
+		key = cv2.waitKey()
 
-"""
-#=================================frame 1==============================================================
-	print "MAKING FIRST FRAME"
-	debug, img = vid.read()
-	H = int(vid.get(4))
-	W = int(vid.get(3))
-
-	current_B = [img[x/W][x%W][0] for x in range(H*W)]
-	current_G = [img[x/W][x%W][1] for x in range(H*W)]
-	current_R = [img[x/W][x%W][2] for x in range(H*W)]
-	#maxB = current_B.index(max(current_B))
-	#maxG = current_G.index(max(current_G))
-	#maxR = current_R.index(max(current_R))
-#===============================frame 2==========================================================================
-	print "MAKING SECOND FRAME"
-	debug, img2 = vid.read()
-	f1_B = [img2[x/W][x%W][0] for x in range(H*W)]	
-	f1_G = [img2[x/W][x%W][1] for x in range(H*W)]
-	f1_R = [img2[x/W][x%W][2] for x in range(H*W)]
-#===============================frame3==========================================================================
-	print "MAKING THIRD FRAME"
-	debug, img3 = vid.read()
-	f2_B = [img3[x/W][x%W][0] for x in range(H*W)]
-	f2_G = [img3[x/W][x%W][0] for x in range(H*W)]
-	f2_R = [img3[x/W][x%W][0] for x in range(H*W)]
-#==============================median=============================================================================
-	print "CALCULATING MEDIAN"
-	B = np.median([current_B, f1_B, f2_B], axis=0)
-	G = np.median([current_G, f1_G, f2_G], axis=0)
-	R = np.median([current_R, f1_R, f2_R], axis=0)	
-	for x in range(H*W):
-		img[x/W][x%W][0] = B[x]
-		img[x/W][x%W][1] = G[x]
-		img[x/W][x%W][2] = R[x]
-	print "DISPLAYING IMAGE"
-	cv2.imshow("",img)
-	cv2.waitKey()
-	cv2.destroyAllWindows()
-	#print H, W, H*W
-
-	#print "max blue", "[%d,%d,%d]" %(current_B[maxB], current_G[maxB], current_R[maxB])
-	#print "max green", "[%d,%d,%d]" %(current_B[maxG], current_G[maxG], current_R[maxG])
-	#print "max red", "[%d,%d,%d]" %(current_B[maxR], current_G[maxR], current_R[maxR])
-"""
 if __name__ == "__main__": main()
