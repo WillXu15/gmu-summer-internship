8a35c45f77d7ebcaa98a578910adabae4d6e79a7
Will Xu <williamxu15@gmail.com>
2013-08-02 13:52:02 -0400

    personRemoval edited

--- code/personRemoval.py
+++ code/personRemoval.py
@@ -1,29 +1,47 @@
+import cv
 import cv2
 import numpy as np
+import time
 
 def main():
-
+	N = 50
 	print "OPENING VIDEO"	
 	vid = cv2.VideoCapture()
 	vid.open("write.avi")
 	print "LOADING VIDEO"
-	imgs = np.asarray([ vid.read()[1] for i in range(int(vid.get(7))) ])
-	vid.release()
+	start = time.clock()
+	imgs = np.asarray([ vid.read()[1] for i in range(N+2) ])
+	print time.clock() - start
 	print "CONCATENATING VIDEO"
-	imgs = np.concatenate( ( np.repeat( imgs[:1], 5, axis = 0 ), imgs, np.repeat( imgs[-1:], 5, axis = 0 ) ), axis = 0 )
-	N = 5
-	print "FINDING MEDIAN"
-	## imgs is a num-frames x num-rows x num-cols x num-channels array.
-	median_frame = np.median( imgs[N-5:N+6], axis=0 )
-	frame_num = 0
-	cv2.imshow(str(frame_num), median_frame.astype(np.uint8))
-	key = cv2.waitKey()
+	imgs = np.concatenate( ( np.repeat( imgs[:1], N, axis = 0 ), imgs  ), axis = 0 )
+	
+	videowriter = cv2.VideoWriter("median.avi", cv.CV_FOURCC("M","J","P","G"), int(vid.get(5)), (int(vid.get(3)),int( vid.get(4))))
 
-	while key != 113:
+	total_frames = int(vid.get(7))
+	print "FINDING MEDIAN"	
+	## imgs is a num-frames x num-rows x num-cols x num-channels array.
+	median_frame = np.median( imgs, axis=0 )
+	frame_num = 0
+	videowriter.write(median_frame.astype(np.uint8))
+
+	while total_frames - frame_num != N:		
+		frame_num += 1
+		imgs[:-1] = imgs[1:]
+		imgs[-1] = vid.read()[1]
+		median_frame = np.median( imgs, axis=0 )
+		videowriter.write(median_frame.astype(np.uint8))
+		print "frame", frame_num, "was writen"
+
+	print "======================================="
+	imgs
+	imgs = np.concatenate( (imgs,  np.repeat( imgs[-1:], N, axis = 0) ), axis=0 )
+	while total_frames-frame_num != 0:
 		frame_num += 1
 		N+=1
-		median_frame = np.median( imgs[N-5:N+6], axis=0 )
-		cv2.imshow(str(frame_num), median_frame.astype(np.uint8) )
-		key = cv2.waitKey()
+		imgs[:-1] = imgs[1:]
+		imgs[-1] = vids.read()[1]
+		median_frame = np.median( imgs, axis = 0)
+		videowriter.write(median_frame.astype(np.uint8))
+		
 
 if __name__ == "__main__": main()
